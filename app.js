7b6ec554f23f495b57090872ade655d256081d48
SmartGlaxx <52786384+SmartGlaxx@users.noreply.github.com>
2021-05-31 17:03:22 +0100

    Add CORS

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ const app = express()
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb+srv://glaxx:'+ process.env.MY_PASSWORD +'@shop-api1.fgv5d.mongodb.net/blog-api?retryWrites=true&w=majority?authSourse=yourDB&w=1', 
+mongoose.connect('mongodb+srv://glaxx:Smeg1984@shop-api1.fgv5d.mongodb.net/blog-api?retryWrites=true&w=majority?authSourse=yourDB&w=1', 
 {useUnifiedTopology : true, useNewUrlParser : true, useCreateIndex : true})
+
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use('/', allRoutes)
 
